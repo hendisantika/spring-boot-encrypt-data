0faae953da85c0658bd628e177e4004b20f0bbea
Hendi Santika <hendisantika@yahoo.co.id>
2022-11-03 15:28:41 +0700

    #123 - Add getByMobile in CustomerController :pencil2: :whale:

--- src/main/java/com/hendisantika/controller/CustomerController.java
+++ src/main/java/com/hendisantika/controller/CustomerController.java
@@ -3,8 +3,11 @@ package com.hendisantika.controller;
 import com.hendisantika.entity.Customer;
 import com.hendisantika.service.CustomerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,12 +22,18 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("customer")
 public class CustomerController {
 
     private final CustomerService customerService;
 
-    @PostMapping("/customer")
+    @PostMapping
     public Customer saveCustomer(@RequestBody Customer customer) {
         return customerService.saveCustomer(customer);
+    }
+
+    @GetMapping("/find/mobile/{mobile}")
+    public Customer getByMobile(@PathVariable String mobile) {
+        return customerService.findByMobile(mobile);
     }
 }
