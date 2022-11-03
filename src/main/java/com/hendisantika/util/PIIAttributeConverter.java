f23efda65e158797a981deedf2cd6acc7f7d9d80
Hendi Santika <hendisantika@yahoo.co.id>
2022-11-03 15:20:30 +0700

    #7 - Add convertToDatabaseColumn in PIIAttributeConverter Util :pencil2: :whale:

--- src/main/java/com/hendisantika/util/PIIAttributeConverter.java
+++ src/main/java/com/hendisantika/util/PIIAttributeConverter.java
@@ -4,9 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 import javax.persistence.AttributeConverter;
 import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 /**
  * Created by IntelliJ IDEA.
@@ -38,5 +44,18 @@ public class PIIAttributeConverter implements AttributeConverter<String, String>
         // IMPORTANT NOTE: Preferably get the secret from the configuration
         // environment.getProperty() etc that loads from Vault or some Secret storage
         key = new SecretKeySpec(SECRET.getBytes(), AES);
+    }
+
+    @Override
+    public String convertToDatabaseColumn(String attribute) {
+        try {
+            Cipher cipher = Cipher.getInstance(AES);
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(attribute.getBytes()));
+        } catch (InvalidKeyException | BadPaddingException | IllegalBlockSizeException | NoSuchPaddingException |
+                 NoSuchAlgorithmException e) {
+            throw new IllegalStateException(e);
+            // You can decide to return an empty or null value on error to be stored if don't want to throw exception
+        }
     }
 }
