4ea1a9e71d6bd6ceb6db22886287c17ed079ab1d
Hendi Santika <hendisantika@yahoo.co.id>
2022-11-03 15:18:35 +0700

    #4 - Add Customer entity :pencil2: :whale:

---
+++ src/main/java/com/hendisantika/entity/Customer.java
@@ -0,0 +1,38 @@
+package com.hendisantika.entity;
+
+import lombok.Data;
+
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-encrypt-data
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/3/22
+ * Time: 15:17
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Data
+@Entity
+@Table(name = "CUSTOMERS")
+public class Customer implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Convert(converter = PIIAttributeConverter.class)
+    private String mobile;
+
+    @Convert(converter = PIIAttributeConverter.class)
+    private String name;
+}
