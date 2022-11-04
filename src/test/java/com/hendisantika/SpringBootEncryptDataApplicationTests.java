383b68050262d37fcff43c938ee2df777a61bf9d
Hendi Santika <hendisantika@yahoo.co.id>
2022-11-04 10:48:58 +0700

    #26 - Add countData Test Case :pencil2: :green_book: :whale:

--- src/test/java/com/hendisantika/SpringBootEncryptDataApplicationTests.java
+++ src/test/java/com/hendisantika/SpringBootEncryptDataApplicationTests.java
@@ -1,6 +1,9 @@
 package com.hendisantika;
 
+import com.hendisantika.entity.Customer;
 import com.hendisantika.repository.CustomerRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,12 +11,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 @SpringBootTest
 class SpringBootEncryptDataApplicationTests {
     private static final Logger LOGGER = LoggerFactory.getLogger(SpringBootEncryptDataApplicationTests.class);
 
     private static final String NAME = "John Doe";
     private static final String EMAIL = "john@example.com";
+
+    private static final String MOBILE = "+6281321411602";
 
     private long id;
 
@@ -23,8 +33,25 @@ class SpringBootEncryptDataApplicationTests {
     @Autowired
     private CustomerRepository customerRepository;
 
-    @Test
-    void contextLoads() {
+    @BeforeEach
+    public void setUp() {
+        Customer customer = Customer.builder()
+                .name(NAME)
+                .mobile(MOBILE)
+                .build();
+        id = customerRepository.save(customer).getId();
     }
+
+    @AfterEach
+    public void delete() {
+        customerRepository.deleteAll();
+    }
+
+    @Test
+    public void countData() {
+        List<Customer> customer = customerRepository.findAll();
+        assertEquals(1, customer.size());
+    }
+
 
 }
