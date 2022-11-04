4244736d6fcad8db22601ba3455dde3cd3bb79c1
Hendi Santika <hendisantika@yahoo.co.id>
2022-11-04 08:58:20 +0700

    #22 - Setup Unit Test Class :pencil2: :green_book: :whale:

--- src/test/java/com/hendisantika/SpringBootEncryptDataApplicationTests.java
+++ src/test/java/com/hendisantika/SpringBootEncryptDataApplicationTests.java
@@ -1,10 +1,27 @@
 package com.hendisantika;
 
+import com.hendisantika.repository.CustomerRepository;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
 class SpringBootEncryptDataApplicationTests {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringBootEncryptDataApplicationTests.class);
+
+    private static final String NAME = "John Doe";
+    private static final String EMAIL = "john@example.com";
+
+    private long id;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private CustomerRepository customerRepository;
 
     @Test
     void contextLoads() {
